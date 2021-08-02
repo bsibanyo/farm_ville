22a08ffb9af6540acefbf4c4fa6de928460e6acb
Window <featherkode@gmail.com>
2021-08-02 15:11:48 +0200

    plus minus

--- cart.php
+++ cart.php
@@ -36,7 +36,7 @@
                                 <div>
                                 <div class="input-group" style="width:130px !important">
                                     <div class="input-group-prepend">
-                                        <button class="btn btn-sm btn-outline-secondary min-qty" type="button" id="button-addon1"><i class="fa fa-minus"></i></button>
+                                        <button class="btn btn-sm btn-outline-secondaryRed min-qty" type="button" id="button-addon1"><i class="fa fa-minus"></i></button>
                                     </div>
                                     <input type="number" class="form-control form-control-sm qty text-center cart-qty" placeholder="" aria-label="Example text with button addon" value="<?php echo $row['quantity'] ?>" aria-describedby="button-addon1" data-id="<?php echo $row['id'] ?>" readonly>
                                     <div class="input-group-append">
