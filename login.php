7a1136e150b03db3089fa2424f612d00dc807cf9
Window <featherkode@gmail.com>
2021-08-02 15:55:23 +0200

    forgotP

--- login.php
+++ login.php
@@ -25,6 +25,7 @@
                 </div>
                 <div class="form-group d-flex justify-content-between">
                     <a href="javascript:void()" id="create_account">Don't have an account?</a>
+                    <a href="javascript:void()" id="create_account">Forgot Password?</a>
                     <button class="btn btn-primary btn-flat">Login</button>
                 </div>
             </form>
