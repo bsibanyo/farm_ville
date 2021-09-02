45af2a10371989684c794db04f8ebdc8f14e97c3
bsibanyo <bsibanyo@student.wethinkcode.co.za>
2021-09-02 10:42:44 +0200

    payment

--- checkout.php
+++ checkout.php
@@ -7,61 +7,8 @@
     endwhile;
     
 ?>
-<?php
-/**
- * @param array $data
- * @param null $passPhrase
- * @return string
- */
-function generateSignature($data, $passPhrase = null) {
-    // Create parameter string
-    $pfOutput = '';
-    foreach( $data as $key => $val ) {
-        if($val !== '') {
-            $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
-        }
-    }
-    // Remove last ampersand
-    $getString = substr( $pfOutput, 0, -1 );
-    if( $passPhrase !== null ) {
-        $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
-    }
-    return md5( $getString );
-}
-
-// Construct variables
-$cartTotal = 10.00;// This amount needs to be sourced from your application
-$data = array(
-    // Merchant details
-    'merchant_id' => '10000100',
-    'merchant_key' => '46f0cd694581a',
-    'return_url' => 'http://google.com',
-    'cancel_url' => 'http://facebook.com',
-    'notify_url' => 'http://facebook.com',
-    // Buyer details
-    'name_first' => 'First Name',
-    'name_last'  => 'Last Name',
-    'email_address'=> 'test@test.com',
-    // Transaction details
-    'm_payment_id' => '1234', //Unique payment ID to pass through to notify_url
-    'amount' => $total,
-    'item_name' => 'Order#123'
-);
-
-$signature = generateSignature($data);
-$data['signature'] = $signature;
-
-// If in testing mode make use of either sandbox.payfast.co.za or www.payfast.co.za
-$testingMode = true;
-$pfHost = $testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
-$htmlForm = '<form action="https://'.$pfHost.'/eng/process" method="post">';
-foreach($data as $name=> $value)
-{
-    $htmlForm .= '<input name="'.$name.'" type="hidden" value=\''.$value.'\' />';
-}
-$htmlForm .= '<input type="submit" class="btn btn-flat btn-dark" value="Pay Now" /></form>';
-?>
-<section class="py-5">
+                        
+    <section class="py-5">
     <div class="container">
         <div class="card rounded-0">
             <div class="card-body"></div>
@@ -145,8 +92,62 @@ $htmlForm .= '<input type="submit" class="btn btn-flat btn-dark" value="Pay Now"
                                 
                                 <b>OR</b>
                                 
-                                <span id="paypal-button"></span>
+                                <!-- <span id="paypal-button"></span> -->
                                 <!-- Payfast -->
+                                <?php
+                        /**
+                         * @param array $data
+                         * @param null $passPhrase
+                         * @return string
+                         */
+                        function generateSignature($data, $passPhrase = null) {
+                            // Create parameter string
+                            $pfOutput = '';
+                            foreach( $data as $key => $val ) {
+                                if($val !== '') {
+                                    $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
+                                }
+                            }
+                            // Remove last ampersand
+                            $getString = substr( $pfOutput, 0, -1 );
+                            if( $passPhrase !== null ) {
+                                $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
+                            }
+                            return md5( $getString );
+                        }
+                        
+                        // Construct variables
+                        $cartTotal = 10.00;// This amount needs to be sourced from your application
+                        $data = array(
+                            // Merchant details
+                            'merchant_id' => '10000100',
+                            'merchant_key' => '46f0cd694581a',
+                            'return_url' => 'http://google.com',
+                            'cancel_url' => 'http://facebook.com',
+                            'notify_url' => 'http://facebook.com',
+                            // Buyer details
+                            'name_first' => 'First Name',
+                            'name_last'  => 'Last Name',
+                            'email_address'=> 'test@test.com',
+                            // Transaction details
+                            'm_payment_id' => '1234', //Unique payment ID to pass through to notify_url
+                            'amount' => $grandTotal,
+                            'item_name' => 'Order#123'
+                        );
+                        
+                        $signature = generateSignature($data);
+                        $data['signature'] = $signature;
+                        
+                        // If in testing mode make use of either sandbox.payfast.co.za or www.payfast.co.za
+                        $testingMode = true;
+                        $pfHost = $testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
+                        $htmlForm = '<form action="https://'.$pfHost.'/eng/process" method="post">';
+                        foreach($data as $name=> $value)
+                        {
+                            $htmlForm .= '<input name="'.$name.'" type="hidden" value=\''.$value.'\' />';
+                        }
+                        $htmlForm .= '<input type="submit" class="btn btn-flat btn-dark" value="Pay Now" /></form>';
+                        ?>
                             </div>
                         </div>
                     </div>
